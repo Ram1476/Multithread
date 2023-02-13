2766daf381638ee57ebdb980fc12abca30db05b0
Ram1476 <ramilangovan.1299@gmail.com>
2023-02-13 12:46:22 +0530

    Add project files.

---
+++ Multithread/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            thread user1 = new thread();
+
+            user1.function1();
+            user1.function2();
+
+            Thread thread1 = new Thread(user1.function1);
+            Thread thread2 = new Thread(user1.function2);
+            thread1.Priority = ThreadPriority.Normal;
+            thread2.Priority = ThreadPriority.Highest;
+            thread1.Start();
+            thread2.Start();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Multithread/thread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithread
+{
+    internal class thread
+    {
+        public void function1() 
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine($"{i} - Funtion-1");
+            }
+        }
+        public void function2() 
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine($"{i} - Funtion-2");
+            }
+        }
+    }
+}
